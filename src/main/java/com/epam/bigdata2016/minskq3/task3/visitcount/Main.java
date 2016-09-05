ad2f5a31d280c66b141ed3c3b682bb29b68bee01
maksimshakavin <exelent.shock@gmail.com>
2016-09-05 11:49:04 +0300

    add Snappy compression

--- src/main/java/com/epam/bigdata2016/minskq3/task3/visitcount/Main.java
+++ src/main/java/com/epam/bigdata2016/minskq3/task3/visitcount/Main.java
@@ -5,6 +5,7 @@ import eu.bitwalker.useragentutils.Browser;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.SnappyCodec;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -36,7 +37,11 @@ public class Main {
         job.setOutputFormatClass(SequenceFileOutputFormat.class);
 
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+
+
         SequenceFileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        SequenceFileOutputFormat.setCompressOutput(job,true);
+        SequenceFileOutputFormat.setOutputCompressorClass(job, SnappyCodec.class);
 
         boolean result = job.waitForCompletion(true);
 
